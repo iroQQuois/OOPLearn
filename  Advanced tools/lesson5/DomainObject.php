c02d20980b493e35c01f0e75ce577adf7d8782fc
iroQQuois <iroqquois@gmail.com>
2020-08-30 16:20:37 +0300

    Позднее статическое связывание

---
+++  Advanced tools/lesson5/DomainObject.php
@@ -0,0 +1,42 @@
+<?php
+
+// Пример позднего статического связывания
+
+abstract class DomainObject
+{
+    private $group;
+
+    public function __construct()
+    {
+        $this->group = static::getGroup();
+    }
+
+    public static function create(): DomainObject
+    {
+        return new static();
+    }
+
+    public static function getGroup(): string
+    {
+        return "default";
+    }
+}
+
+class User extends DomainObject
+{
+}
+
+class Document extends DomainObject
+{
+    public static function getGroup(): string
+    {
+        return "document";
+    }
+}
+
+class SpreadSheet extends Document
+{
+}
+
+var_dump(Document::create());
+var_dump(SpreadSheet::create());
